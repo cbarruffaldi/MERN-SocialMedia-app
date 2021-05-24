eeeb119be3157420937b2a23adbb0e3fc49fe6ce
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-05-24 18:20:47 +0200

    Follow user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -51,9 +51,31 @@ router.get("/:id", async (req, res) => {
     } catch {
         res.status(500).json(err);
     }
-})
-// Follow user
+});
 
+// Follow user
+router.put("/:id/follow", async (req,res) => {
+    if(req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+
+            if(!user.followers.includes(req.body.userId)) {
+                await user.updateOne({$push: {followers: req.body.userId}});
+                await currentUser.updateOne({$push: {following: req.params.id}});
+                res.status(200).json("User has been followed");
+            } else {
+                res.status(401).json("You calready follow this user");
+            }
+
+        } catch {
+            res.status(500).json(err);
+        }
+
+    } else {
+        res.status(403).json("You can´t follow yourself");
+    }
+})
 //Unfollow user
 
 module.exports = router;
