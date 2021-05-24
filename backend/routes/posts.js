830777e81e4ffe5528942c637f35c8de99e156ae
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-05-24 19:03:32 +0200

    Get Post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -65,6 +65,14 @@ router.put("/:id/like", async (req, res) => {
 })
 
 // Get a post
+router.get("/:id", async (req, res) =>{
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+} )
 
 // Get timeline posts (all posts of users followed)
 
