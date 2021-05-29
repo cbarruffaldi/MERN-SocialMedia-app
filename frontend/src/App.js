1f865aa1e167a58218cca13af8690bc6a2a989a1
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-05-29 13:01:43 +0200

    Rightbar profile/Home logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,6 @@
 import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";
-import Login from "./pages/login/Login"
-import Register from "./pages/register/Register"
-
 
 function App() {
-  return <Register></Register>
+  return <Home></Home>
 }
 export default App;
